9583d9991c7b07c1c4358d301abb905cb33d654b
exigentdev <exigentdev@gmail.com>
2025-02-13 16:18:51 -0600

    good starting point for running in vscode

--- ExigentDev.DIM.Api/Program.cs
+++ ExigentDev.DIM.Api/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,33 +11,52 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  app.MapOpenApi();
+  app.MapScalarApiReference(options =>
+  {
+    options
+      .WithTitle("Weather Forecast API")
+      .WithTheme(ScalarTheme.Saturn)
+      .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+  });
 }
 
 app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+  "Freezing",
+  "Bracing",
+  "Chilly",
+  "Cool",
+  "Mild",
+  "Warm",
+  "Balmy",
+  "Hot",
+  "Sweltering",
+  "Scorching",
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
+app.MapGet(
+    "/weatherforecast",
+    () =>
+    {
+      var forecast = Enumerable
+        .Range(1, 5)
+        .Select(index => new WeatherForecast(
+          DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+          Random.Shared.Next(-20, 55),
+          summaries[Random.Shared.Next(summaries.Length)]
         ))
         .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+      return forecast;
+    }
+  )
+  .WithName("GetWeatherForecast");
 
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+  public int TemperatureF => 132 + (int)(TemperatureC / 0.5556);
 }
--- ExigentDev.DIM.Api/obj/Debug/net9.0/ExigentDev.DIM.Api.AssemblyInfo.cs
+++ ExigentDev.DIM.Api/obj/Debug/net9.0/ExigentDev.DIM.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExigentDev.DIM.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+299070c871146ef6aa029e8991532bbf17cca567")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExigentDev.DIM.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExigentDev.DIM.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
