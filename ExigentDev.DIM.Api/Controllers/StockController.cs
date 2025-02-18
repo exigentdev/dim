db4cdcf8fb3e028746b667fff73cec98ea65e70c
exigentdev <exigentdev@gmail.com>
2025-02-18 14:33:05 -0600

    Added other crud methods

--- ExigentDev.DIM.Api/Controllers/StockController.cs
+++ ExigentDev.DIM.Api/Controllers/StockController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ExigentDev.DIM.Api.Data;
+using ExigentDev.DIM.Api.Dtos.Stock;
+using ExigentDev.DIM.Api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExigentDev.DIM.Api.Controllers
@@ -16,7 +18,7 @@ namespace ExigentDev.DIM.Api.Controllers
     [HttpGet]
     public IActionResult GetAll()
     {
-      var stocks = _context.Stocks.ToList();
+      var stocks = _context.Stocks.ToList().Select(s => s.ToStockDto());
 
       return Ok(stocks);
     }
@@ -31,7 +33,50 @@ namespace ExigentDev.DIM.Api.Controllers
         return NotFound();
       }
 
-      return Ok(stock);
+      return Ok(stock.ToStockDto());
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+    {
+      var stockModel = stockDto.ToStockFromCreateDTO();
+
+      _context.Stocks.Add(stockModel);
+      _context.SaveChanges();
+
+      return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+    {
+      var stockModel = _context.Stocks.Find(id);
+
+      if (stockModel == null)
+      {
+        return NotFound();
+      }
+
+      _context.Entry(stockModel).CurrentValues.SetValues(updateDto);
+      _context.SaveChanges();
+
+      return Ok(stockModel.ToStockDto());
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+      var stockModel = _context.Stocks.Find(id);
+
+      if (stockModel == null)
+      {
+        return NotFound();
+      }
+
+      _context.Stocks.Remove(stockModel);
+      _context.SaveChanges();
+
+      return NoContent();
     }
   }
 }
