7b59fefe2dbab6af13eb9a2e25e914207e41216f
exigentdev <exigentdev@gmail.com>
2025-03-01 21:46:01 -0600

    added migrations for previous commit

---
+++ ExigentDev.DIM.Api/Migrations/20250302034209_PortfolioManyToMany.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExigentDev.DIM.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PortfolioManyToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Stocks_StockId",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Stocks",
+                table: "Stocks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "Stocks",
+                newName: "Stock");
+
+            migrationBuilder.RenameTable(
+                name: "Comments",
+                newName: "Comment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_StockId",
+                table: "Comment",
+                newName: "IX_Comment_StockId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Stock",
+                table: "Stock",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Portfolio",
+                columns: table => new
+                {
+                    AppUserId = table.Column<string>(type: "text", nullable: false),
+                    StockId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolio", x => new { x.AppUserId, x.StockId });
+                    table.ForeignKey(
+                        name: "FK_Portfolio_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Portfolio_Stock_StockId",
+                        column: x => x.StockId,
+                        principalTable: "Stock",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Portfolio_StockId",
+                table: "Portfolio",
+                column: "StockId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Stock_StockId",
+                table: "Comment",
+                column: "StockId",
+                principalTable: "Stock",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Stock_StockId",
+                table: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "Portfolio");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Stock",
+                table: "Stock");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment");
+
+            migrationBuilder.RenameTable(
+                name: "Stock",
+                newName: "Stocks");
+
+            migrationBuilder.RenameTable(
+                name: "Comment",
+                newName: "Comments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_StockId",
+                table: "Comments",
+                newName: "IX_Comments_StockId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Stocks",
+                table: "Stocks",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Stocks_StockId",
+                table: "Comments",
+                column: "StockId",
+                principalTable: "Stocks",
+                principalColumn: "Id");
+        }
+    }
+}
